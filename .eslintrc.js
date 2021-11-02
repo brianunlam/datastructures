f3cc1ff4adc382479277a350ff1a241f89fd5a3a
Hugo Venega <hugo.venega.00>
2021-11-02 10:15:02 -0300

    Add necessary files configs

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
