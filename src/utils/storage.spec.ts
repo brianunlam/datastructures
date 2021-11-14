23ad0a587aa7f3e330ebaa63dab55a1baf0607d4
Hugo Venega <hugo.venega.00>
2021-11-14 19:48:05 -0300

    Add Person interface

--- src/utils/storage.spec.ts
+++ src/utils/storage.spec.ts
@@ -1,26 +1,30 @@
 import Storage from './storage';
 
+interface Person {
+  name: string
+}
+
 describe('Storage', () => {
-  let storage: Storage<String>;
+  let storage: Storage<Person>;
   beforeEach(() => {
-    storage = new Storage<String>();
+    storage = new Storage<Person>();
   });
   it('should add an element in the storage', () => {
-    const saved = storage.save('santiago');
-    expect(storage.get(saved)).toEqual('santiago');
+    const saved = storage.save({ name: 'santiago' });
+    expect(storage.get(saved)).toEqual({ name: 'santiago' });
   });
 
   it('should remove from storage', () => {
     const toAdd = ['marcos', 'thiago'];
-    toAdd.forEach((element) => storage.save(element));
-    const ixForDelete = storage.save('santiago');
-    expect(storage.delete(ixForDelete)).toEqual('santiago');
+    toAdd.forEach((element) => storage.save({ name: element }));
+    const ixForDelete = storage.save({ name: 'santiago' });
+    expect(storage.delete(ixForDelete)).toEqual({ name: 'santiago' });
   });
 
   it('should Get from storage', () => {
     const toAdd = ['marcos', 'thiago'];
-    toAdd.forEach((element) => storage.save(element));
-    const ixForGet = storage.save('santiago');
-    expect(storage.get(ixForGet)).toEqual('santiago');
+    toAdd.forEach((element) => storage.save({ name: element }));
+    const ixForGet = storage.save({ name: 'santiago' });
+    expect(storage.get(ixForGet)).toEqual({ name: 'santiago' });
   });
 });
