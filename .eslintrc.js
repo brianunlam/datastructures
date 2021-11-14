c3cf02bb3059cf56ed9c9f98a08e70c4b1ff2d66
Hugo Venega <hugo.venega.00>
2021-11-14 19:06:52 -0300

    Fix identation

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,10 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['airbnb-base',
-  'airbnb-typescript/base'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json'
