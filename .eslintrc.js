c7c5c5b179e674a2159dd56b4252c950676ef072
Hugo Venega <hugo.venega.00>
2021-11-14 18:49:10 -0300

    Add basic configuration

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base',
+  'airbnb-typescript/base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+   },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
