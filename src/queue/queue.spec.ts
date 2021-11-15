67a9ad1f22d5a1ab7a41098d105fcdf1c682767a
Hugo Venega <hugo.venega.00>
2021-11-15 17:41:59 -0300

    Improve tests titles

--- src/queue/queue.spec.ts
+++ src/queue/queue.spec.ts
@@ -1,39 +1,33 @@
 import Queue from './queue';
 
 describe('Queue', () => {
-  let q: Queue<String>;
-  const insertTestElement = (string: string) => q.enQueue(string);
+  let queue: Queue<String>;
+  const insertTestElement = (string: string) => queue.enQueue(string);
   beforeEach(() => {
-    q = new Queue<String>();
+    queue = new Queue<String>();
   });
-  it('should add an item to queue', () => {
-    q.enQueue('sam');
-    expect(q.length).toEqual(1);
-  });
-
-  it('should add multiple items to queue', () => {
-    const items = ['sara', 'jona', 'lucas'];
-    items.forEach(insertTestElement);
-    expect(q.length).toEqual(3);
-  });
-
-  it('should remove item from empty queue', () => {
-    expect(q.deQueue()).toBe(null);
-  });
-
-  it('should add multiple items to queue', () => {
-    const items = ['sara', 'jona', 'lucas'];
-    items.forEach(insertTestElement);
-    expect(q.deQueue()).toEqual('lucas');
+  it('should add an item to an empty queue', () => {
+    queue.enQueue('sam');
+    expect(queue.length).toEqual(1);
   });
 
   it('should add multiple items to the queue', () => {
-    const items = ['sara', 'lucas'];
+    const items = ['sara', 'jona', 'lucas'];
     items.forEach(insertTestElement);
-    expect(q.deQueue()).toEqual('lucas');
+    expect(queue.length).toEqual(3);
+  });
+
+  it('should return null when removing an element of empty queue', () => {
+    expect(queue.deQueue()).toBe(null);
+  });
+
+  it('should remove the last element from the queue', () => {
+    const items = ['sara', 'jona', 'lucas'];
+    items.forEach(insertTestElement);
+    expect(queue.deQueue()).toEqual('lucas');
   });
 
   it('should return isEmpty = true', () => {
-    expect(q.isEmpty()).toBe(true);
+    expect(queue.isEmpty()).toBe(true);
   });
 });
