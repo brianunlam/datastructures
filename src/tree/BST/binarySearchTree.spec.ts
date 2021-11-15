6024ff25ffcc1c563fbfe4920070de2ac00abcb1
Hugo Venega <hugo.venega.00>
2021-11-15 18:41:29 -0300

    Fix test titles

--- src/tree/BST/binarySearchTree.spec.ts
+++ src/tree/BST/binarySearchTree.spec.ts
@@ -4,46 +4,40 @@ interface Nn {
   n: number;
 }
 
-describe('testing binarySearchTree class with numbers', () => {
+describe('binarySearchTree', () => {
   let tree: BinarySearchTree<Nn>;
   beforeEach(() => {
     tree = new BinarySearchTree<Nn>((a: Nn, b: Nn) => a.n - b.n);
   });
 
-  it('I should add the element to the empty tree', () => {
+  it('should add the element to the empty tree', () => {
     tree.add({ n: 12 });
     expect(tree.length).toEqual(1);
   });
 
-  it('I should add multiple elements to the tree', () => {
+  it('should add multiple elements to the tree', () => {
     const treeElements = [14, 8, 15, 10, 3];
     treeElements.forEach((n) => tree.add({ n }));
     expect(tree.length).toEqual(5);
   });
 
-  it('I should add a repeating element to the tree', () => {
+  it('I should return false when added repeating element to the tree', () => {
     const treeElements = [14, 8, 15, 10, 3];
     treeElements.forEach((n) => tree.add({ n }));
     expect(tree.add({ n: 8 })).toBe(false);
   });
 
-  it('I should remove an element to the empty tree', () => {
+  it('should return null when remove an element to the empty tree', () => {
     expect(tree.delete({ n: 12 })).toEqual(null);
   });
 
-  it('I should remove an element to the tree', () => {
+  it('should remove an element to the tree', () => {
     const treeElements = [14, 8, 15, 10, 3];
     treeElements.forEach((n) => tree.add({ n }));
     expect(tree.delete({ n: 3 })).toEqual({ n: 3 });
   });
 
-  it('I should remove an element to the tree', () => {
-    const treeElements = [14, 8, 15, 10, 3];
-    treeElements.forEach((n) => tree.add({ n }));
-    expect(tree.delete({ n: 10 })).toEqual({ n: 10 });
-  });
-
-  it('I should remove an non-existent element to the tree', () => {
+  it('should return null when remove an element when doesn`t exist', () => {
     const treeElements = [14, 8, 15, 10, 3];
     treeElements.forEach((n) => tree.add({ n }));
     expect(tree.delete({ n: 177 })).toEqual(null);
@@ -56,7 +50,7 @@ describe('testing binarySearchTree class with numbers', () => {
     expect(tree.numberOfNodes).toEqual(3);
   });
 
-  it('it should return null in an empty tree', () => {
+  it('should return null when search in an empty tree', () => {
     expect(tree.search({ n: 12 })).toEqual(null);
   });
 
