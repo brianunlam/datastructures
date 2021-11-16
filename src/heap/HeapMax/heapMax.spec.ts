1ce4e75426cebe9d5653c8e4c504d605dffc94f9
Hugo Venega <hugo.venega.00>
2021-11-16 10:06:30 -0300

    Improve tests interfaces and titles

--- src/heap/HeapMax/heapMax.spec.ts
+++ src/heap/HeapMax/heapMax.spec.ts
@@ -1,13 +1,14 @@
 import HeapOfMax from './heapMax';
 
-interface Nn {
+interface TestElement {
   n: number;
 }
 
-describe('Heap of Max tests of type Object', () => {
-  let myHeap: HeapOfMax<Nn>;
+describe('Heap of Max', () => {
+  let myHeap: HeapOfMax<TestElement>;
+  const compareFunction = (a: TestElement, b: TestElement) => a.n - b.n;
   beforeEach(() => {
-    myHeap = new HeapOfMax<Nn>((a: Nn, b: Nn) => a.n - b.n);
+    myHeap = new HeapOfMax<TestElement>(compareFunction);
   });
 
   it('Should insert a new element into empty Heap', () => {
@@ -15,7 +16,7 @@ describe('Heap of Max tests of type Object', () => {
     expect(myHeap.length).toEqual(1);
   });
 
-  it('Should insert a new element into the Heap and up it', () => {
+  it('Should insert a new element into the Heap', () => {
     const expectedHeap = [{ n: 16 }, { n: 12 }];
     myHeap.insert({ n: 12 });
     myHeap.insert({ n: 16 });
@@ -23,18 +24,18 @@ describe('Heap of Max tests of type Object', () => {
   });
 
   it('Should delete element into the Heap', () => {
-    const toCharge = [{ n: 12 }, { n: 14 }, { n: 16 }, { n: 21 }, { n: 30 }, { n: 15 }, { n: 70 },
+    const toInsert = [{ n: 12 }, { n: 14 }, { n: 16 }, { n: 21 }, { n: 30 }, { n: 15 }, { n: 70 },
       { n: 54 }, { n: 31 }, { n: 65 }, { n: 49 }, { n: 27 }, { n: 55 }, { n: 2 }];
     const expectedHeap = [{ n: 70 }, { n: 54 }, { n: 55 }, { n: 27 }, { n: 49 }, { n: 31 },
       { n: 30 }, { n: 2 }, { n: 21 }, { n: 16 }, { n: 15 }, { n: 14 }, { n: 12 }];
-    const myHeap2 = HeapOfMax.fromArray<Nn>(toCharge, (a: Nn, b: Nn) => a.n - b.n);
-    myHeap2.remove({ n: 65 });
-    expect((myHeap2 as any).memory).toEqual(expectedHeap);
+    myHeap = HeapOfMax.fromArray<TestElement>(toInsert, compareFunction);
+    myHeap.remove({ n: 65 });
+    expect((myHeap as any).memory).toEqual(expectedHeap);
   });
 
   it('Should return the max of the Heap', () => {
-    const toCharge = [{ n: 12 }, { n: 14 }, { n: 16 }];
-    const myHeap2: HeapOfMax<Nn> = HeapOfMax.fromArray<Nn>(toCharge, (a: Nn, b: Nn) => a.n - b.n);
-    expect(myHeap2.extractMax()).toEqual({ n: 16 });
+    const toInsert = [{ n: 12 }, { n: 14 }, { n: 16 }];
+    myHeap = HeapOfMax.fromArray<TestElement>(toInsert, compareFunction);
+    expect(myHeap.extractMax()).toEqual({ n: 16 });
   });
 });
--- src/heap/HeapMin/heapMin.spec.ts
+++ src/heap/HeapMin/heapMin.spec.ts
@@ -1,13 +1,14 @@
 import HeapOfMin from './heapMin';
 
-interface Nn {
+interface TestElement {
   n: number;
 }
 
-describe('Heap of Mins tests of type Object', () => {
-  let myHeap: HeapOfMin<Nn>;
+describe('Heap of Mins', () => {
+  let myHeap: HeapOfMin<TestElement>;
+  const compareFunction = (a: TestElement, b: TestElement) => a.n - b.n;
   beforeEach(() => {
-    myHeap = new HeapOfMin<Nn>((a: Nn, b: Nn) => a.n - b.n);
+    myHeap = new HeapOfMin<TestElement>(compareFunction);
   });
 
   it('Should insert a new element into empty Heap', () => {
@@ -15,26 +16,26 @@ describe('Heap of Mins tests of type Object', () => {
     expect(myHeap.length).toEqual(1);
   });
 
-  it('Should insert a new element into the Heap and up it', () => {
+  it('Should insert a new element into the Heap', () => {
     const expectedHeap = [{ n: 12 }, { n: 16 }];
-    myHeap.insert({ n: 12 });
     myHeap.insert({ n: 16 });
+    myHeap.insert({ n: 12 });
     expect((myHeap as any).memory).toEqual(expectedHeap);
   });
 
   it('Should delete element into the Heap', () => {
-    const toCharge = [{ n: 12 }, { n: 14 }, { n: 16 }, { n: 21 }, { n: 30 }, { n: 15 }, { n: 70 },
+    const toInsert = [{ n: 12 }, { n: 14 }, { n: 16 }, { n: 21 }, { n: 30 }, { n: 15 }, { n: 70 },
       { n: 54 }, { n: 31 }, { n: 65 }, { n: 49 }, { n: 27 }, { n: 55 }, { n: 2 }];
     const expectedHeap = [{ n: 2 }, { n: 12 }, { n: 14 }, { n: 15 }, { n: 16 }, { n: 21 },
       { n: 27 }, { n: 30 }, { n: 31 }, { n: 49 }, { n: 54 }, { n: 55 }, { n: 70 }];
-    const myHeap2 = HeapOfMin.fromArray<Nn>(toCharge, (a: Nn, b: Nn) => a.n - b.n);
-    myHeap2.remove({ n: 65 });
-    expect((myHeap2 as any).memory).toEqual(expectedHeap);
+    myHeap = HeapOfMin.fromArray<TestElement>(toInsert, compareFunction);
+    myHeap.remove({ n: 65 });
+    expect((myHeap as any).memory).toEqual(expectedHeap);
   });
 
-  it('Should return the max of the Heap', () => {
-    const toCharge = [{ n: 12 }, { n: 14 }, { n: 16 }];
-    const myHeap2: HeapOfMin<Nn> = HeapOfMin.fromArray<Nn>(toCharge, (a: Nn, b: Nn) => a.n - b.n);
-    expect(myHeap2.extractMin()).toEqual({ n: 12 });
+  it('Should return the min of the Heap', () => {
+    const toInsert = [{ n: 12 }, { n: 14 }, { n: 16 }];
+    myHeap = HeapOfMin.fromArray<TestElement>(toInsert, compareFunction);
+    expect(myHeap.extractMin()).toEqual({ n: 12 });
   });
 });
