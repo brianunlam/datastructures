d30026ef705c58364d70dc5265a9b6a17637a60c
Hugo Venega <hugo.venega.00>
2021-11-18 15:22:29 -0300

    Separate functions of MergeSort tests

--- src/sort/merge/mergeSort.spec.ts
+++ src/sort/merge/mergeSort.spec.ts
@@ -1,15 +1,18 @@
 import mergeSort from './mergeSort';
 
+const sortLowToHigh = (a: number, b: number): boolean => (a > b);
+const sortHighToLow = (a: number, b: number): boolean => (a < b);
+
 describe('Merge sort', () => {
   it('should return an array sorted from lowest to highest', () => {
     const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
     const expectedArray = [1, 9, 12, 21, 33, 43, 44, 65, 67];
-    expect(mergeSort(arrayToSort, (a, b): boolean => (a > b))).toEqual(expectedArray);
+    expect(mergeSort(arrayToSort, sortLowToHigh)).toEqual(expectedArray);
   });
 
   it('should return an array sorted from highest to lowest', () => {
     const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
     const expectedArray = [67, 65, 44, 43, 33, 21, 12, 9, 1];
-    expect(mergeSort(arrayToSort, (a, b): boolean => (a < b))).toEqual(expectedArray);
+    expect(mergeSort(arrayToSort, sortHighToLow)).toEqual(expectedArray);
   });
 });
