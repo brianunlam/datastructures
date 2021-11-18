3f773ddb6a1640ed945699bef02b879564212a74
Hugo Venega <hugo.venega.00>
2021-11-18 15:08:45 -0300

    Fix uppercases in quicksort and improve tests

--- src/sort/quick/quickSort.spec.ts
+++ src/sort/quick/quickSort.spec.ts
@@ -1,17 +1,27 @@
 import quickSort from './quickSort';
 
+const sortLowToHigh = (a: number, b: number): boolean => (a < b);
+const sortHighToLow = (a: number, b: number): boolean => (a > b);
+
 describe('quick sort', () => {
-  it('It should return an array sorted from lowest to highest', () => {
-    const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
-    const expectedArray = [1, 9, 12, 21, 33, 43, 44, 65, 67];
-    expect(quickSort(arrayToSort, (a: number, b: number): boolean => (a < b)))
+  it('should return an array with two elements sorted from lowest to highest', () => {
+    const arrayToSort = [65, 43];
+    const expectedArray = [43, 65];
+    expect(quickSort(arrayToSort, sortLowToHigh))
       .toEqual(expectedArray);
   });
 
-  it('It should return an array sorted from highest to lowest', () => {
+  it('should return an array sorted from lowest to highest', () => {
+    const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
+    const expectedArray = [1, 9, 12, 21, 33, 43, 44, 65, 67];
+    expect(quickSort(arrayToSort, sortLowToHigh))
+      .toEqual(expectedArray);
+  });
+
+  it('should return an array sorted from highest to lowest', () => {
     const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
     const expectedArray = [67, 65, 44, 43, 33, 21, 12, 9, 1];
-    expect(quickSort(arrayToSort, (a: number, b: number): boolean => (a > b)))
+    expect(quickSort(arrayToSort, sortHighToLow))
       .toEqual(expectedArray);
   });
 });
