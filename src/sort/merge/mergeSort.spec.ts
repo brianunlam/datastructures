2209bd81133777d23fe8a6b01102c261cdcbdb52
Hugo Venega <hugo.venega.00>
2021-11-18 13:29:27 -0300

    Add mergeSort

---
+++ src/sort/merge/mergeSort.spec.ts
@@ -0,0 +1,15 @@
+import mergeSort from './mergeSort';
+
+describe('Merge sort', () => {
+  it('It should return an array sorted from lowest to highest', () => {
+    const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
+    const expectedArray = [1, 9, 12, 21, 33, 43, 44, 65, 67];
+    expect(mergeSort(arrayToSort, (a, b): boolean => (a > b))).toEqual(expectedArray);
+  });
+
+  it('It should return an array sorted from highest to lowest', () => {
+    const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
+    const expectedArray = [67, 65, 44, 43, 33, 21, 12, 9, 1];
+    expect(mergeSort(arrayToSort, (a, b): boolean => (a < b))).toEqual(expectedArray);
+  });
+});
