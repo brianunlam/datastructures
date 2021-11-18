5b91e988f39abac2c9541e8cf393945d4b0bf8f6
Hugo Venega <hugo.venega.00>
2021-11-18 15:20:17 -0300

    Fix name function and improve tests names

--- src/sort/insertion/insertionSort.spec.ts
+++ src/sort/insertion/insertionSort.spec.ts
@@ -1,15 +1,18 @@
 import insertionSort from './insertionSort';
 
+const sortLowToHigh = (a: number, b: number): boolean => (a > b);
+const sortHighToLow = (a: number, b: number): boolean => (a < b);
+
 describe('Insertion sort', () => {
-  it('It should return an array sorted from lowest to highest', () => {
+  it('should return an array sorted from lowest to highest', () => {
     const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
     const expectedArray = [1, 9, 12, 21, 33, 43, 44, 65, 67];
-    expect(insertionSort(arrayToSort, (a, b): boolean => (a > b))).toEqual(expectedArray);
+    expect(insertionSort(arrayToSort, sortLowToHigh)).toEqual(expectedArray);
   });
 
-  it('It should return an array sorted from highest to lowest', () => {
+  it('should return an array sorted from highest to lowest', () => {
     const arrayToSort = [43, 65, 44, 12, 67, 1, 9, 33, 21];
     const expectedArray = [67, 65, 44, 43, 33, 21, 12, 9, 1];
-    expect(insertionSort(arrayToSort, (a, b): boolean => (a < b))).toEqual(expectedArray);
+    expect(insertionSort(arrayToSort,sortHighToLow)).toEqual(expectedArray);
   });
 });
